3fe365ceda587431431b086144ea929e5b277837
root <root@ip-172-31-42-248.us-east-2.compute.internal>
2025-11-06 10:09:49 +0000

    java code added

---
+++ app.java
@@ -0,0 +1,39 @@
+/**
+ * A simple Java program that prints a message to the console,
+ * defines two variables, and performs a simple calculation.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // --- Part 1: Printing a basic message ---
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java example.");
+
+        // --- Part 2: Working with variables ---
+
+        // Declare an integer variable to store a whole number
+        int numberA = 10;
+
+        // Declare another integer variable
+        int numberB = 5;
+
+        // Declare a third variable to store the result of an operation
+        int sum = numberA + numberB;
+
+        // --- Part 3: Displaying the results ---
+
+        System.out.println("The value of numberA is: " + numberA);
+        System.out.println("The value of numberB is: " + numberB);
+        
+        // Print the result of the addition
+        System.out.println("The sum of A and B is: " + sum);
+        
+        // Example of a conditional statement
+        if (sum > 10) {
+            System.out.println("The sum is greater than 10.");
+        } else {
+            System.out.println("The sum is 10 or less.");
+        }
+    }
+}
+
